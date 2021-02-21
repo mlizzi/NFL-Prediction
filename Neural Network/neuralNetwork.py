660833defd8a7f0e9056d0cd7d3314be40d1cb5e
MLizzi <michael.lizzi@hotmail.com>
2021-02-20 20:02:44 -0500

    Updated code to run training as desired, test data still includes current stats

--- Neural Network/neuralNetwork.py
+++ Neural Network/neuralNetwork.py
@@ -7,6 +7,9 @@ from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 import datetime as dt
 from matplotlib import pyplot as plt
+# from torchsummary import summary
+
+SEASON_TO_YEAR = dict(zip(range(1970, 2020), range(51,101)))
 
 class neuralNetwork(nn.Module):
 
@@ -28,104 +31,130 @@ class neuralNetwork(nn.Module):
 
 class NFLDataset(Dataset):
 
-    def __init__(self, data):
-        self.data = torch.from_numpy(data.to_numpy(dtype=np.float32))
-
+    def __init__(self, dataFrame):
+        self.data = torch.tensor(dataFrame.drop('winnerATS', axis=1).values, dtype=torch.float)
+        self.target = torch.tensor(dataFrame['winnerATS'].values, dtype=torch.float).unsqueeze(1)
     # This gets one row, ground truth https://pytorch.org/docs/stable/_modules/torchvision/datasets/mnist.html#MNIST
     def __getitem__(self, index):
-        return (self.data[index][1:], self.data[index][0].unsqueeze(0))
+        return (self.data[index], self.target[index])
 
     def __len__(self):
         return self.data.shape[0]
 
+def getSeasonData(database, startSeason, endSeason):
+    startSeasonYear = SEASON_TO_YEAR[startSeason]
+    endSeasonYear = SEASON_TO_YEAR[endSeason]
 
+    return database.loc[startSeasonYear: endSeasonYear]
 if __name__ == '__main__':
 
-    data = ['RushYds', 'PassTDs', 'Sacks', 'Fumbles']
+
+    gameStats = ['seasonNumber', 'winnerATS']
+    teamStats = ['PassYds', 'RushYds', 'PassTDs', 'Sacks', 'Turnovers']
+
+    normalizedStats = teamStats
+
+    normalizedColumns = []
+    dataColumns = []
+    for info in teamStats:
+        for loc in ['home', 'away']:
+            dataColumns.append(loc + info)
+
+    for info in normalizedStats:
+        for loc in ['home', 'away']:
+            normalizedColumns.append(loc + info)
+
+
+
+    dataColumns += gameStats
 
     filePath = os.path.join(os.getcwd(), 'pp_master_boxscore_data_1970_2019.csv')
 
 
-    database = pd.read_csv(filePath, parse_dates=['Date'], index_col='seasonNumber', usecols=)
+    # database = pd.read_csv(filePath, index_col=['seasonNumber'], usecols=dataColumns)
+    database = pd.read_csv(filePath, usecols=dataColumns, index_col='seasonNumber')
 
-    tempDatabase = database[database.columns.difference(['Home Line Close', 'Home Score', 'Away Score', 'Home Yards nonnorm', 'Away Yards nonnorm', 'Home Team', 'Away Team', 'Playoff Game?'])]
-    database = tempDatabase
+    for info in normalizedColumns:
+        database[info] = (database[info] - database[info].mean()) / database[info].std()
 
-    trainStartYear = 2006
-    trainEndYear = 2007
-    trainData = database.loc[str(trainStartYear): str(trainEndYear)]
-    testStartYear = 2008
-    testEndYear = 2009
-    testData = database.loc[str(testStartYear):str(testEndYear)]
-    # testData = database.loc[database.index.isin(testGames)]
+    trainData = getSeasonData(database, 2002, 2003)
+    testData = getSeasonData(database, 2004, 2004)
 
     trainLoader = DataLoader(NFLDataset(trainData), batch_size=len(trainData))
     testLoader = DataLoader(NFLDataset(testData), batch_size=len(testData))
 
-    fig, axs = plt.subplots(2,1, figsize=(10,10))
-    for t in range(5): # Testing
-        model = neuralNetwork(5, 3, 2, 1)
+    model = neuralNetwork(10, 5, 2, 1)
 
-        # loss = nn.CrossEntropyLoss()
-        # https://www.youtube.com/watch?v=wpPkDSMzdKo>
-        loss = nn.BCELoss()
-        learning_rate = 0.01
-        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, nesterov=True, momentum=0.9, dampening=0)
-        x = nn.Sigmoid()
-        error = []
-        testAccuracy = []
-        for i in range(1500):
-            for j, (data, target) in enumerate(trainLoader):
-                optimizer.zero_grad()
-                pred = model(data)
-                pred = x(pred)
+    # loss = nn.CrossEntropyLoss()
+    # https://www.youtube.com/watch?v=wpPkDSMzdKo>
 
-                output = loss(pred, target)
-                #output.item to save loss
+    # weight = torch.tensor(np.arange(0, 1, 1/777)).unsqueeze(1)
+    # loss = nn.BCELoss(weight=weight)
+    loss = nn.BCELoss()
+    learning_rate = 0.012
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, nesterov=True, momentum=0.9, dampening=0)
+    x = nn.Sigmoid()
+    error = []
+    testAccuracy = []
+    outputLoss = []
+    for i in range(4000):
+        for j, (data, target) in enumerate(trainLoader):
 
-                output.backward()
-                optimizer.step()
+            optimizer.zero_grad()
+            pred = model(data)
+            pred = x(pred)
+            output = loss(pred, target)
+            #output.item to save loss
+            outputLoss.append(output)
+            output.backward()
+            optimizer.step()
 
-            error.append(torch.mean((torch.round(pred) == target).float()))
+        error.append(torch.mean((torch.round(pred) == target).float()))
 
+        if i % 100 == 0:
+            print("Training Loss: " + str(output.item()))
 
+    plt.plot(error)
+    plt.plot(outputLoss)
+    plt.legend(['Training Accuracy', 'Training Loss'])
+    plt.savefig('Test 8')
+    total = 0
+    correct = 0
 
-        total = 0
-        correct = 0
-        with torch.no_grad():
-            for i, (data, target) in enumerate(testLoader):
+    with torch.no_grad():
+        for i, (data, target) in enumerate(testLoader):
 
-                pred = model(data)
-                pred = x(pred)
-                output = loss(pred, target)
-                total += target.size(0)
-                pred = torch.round(pred)
-                correct += (pred == target).sum().item()
+            pred = model(data)
+            pred = x(pred)
+            output = loss(pred, target)
+            total += target.size(0)
+            pred = torch.round(pred)
+            correct += (pred == target).sum().item()
 
-        accuracy = correct / total
+    accuracy = correct / total
+#
+    print(accuracy)
+#     axs[0].plot(error, label='Model #' + str(t+1))
+#     axs[0].set_xlabel('Epochs')
+#     axs[0].set_ylabel('Training Accuracy')
+#
+#     axs[1].scatter(t+1, accuracy, label='Model #' + str(t+1))
+#     axs[1].set_xlabel('Model #')
+#     axs[1].set_ylabel('Test Accuracy')
+#
+#
+#     # testAccuracy.append(accuracy)
+#     print('Accuracy of the network on test set: Games from {0}-{1}: {2}%'.format(testStartYear, testEndYear, accuracy))
+#
+#
+# axs[0].legend(loc='upper left', bbox_to_anchor=(1.01, 1))
+# axs[1].legend(loc='upper left', bbox_to_anchor=(1.01, 1))
+#
+# axs[0].title.set_text('Training accuracy for different models (Games from {0}-{1})'.format(trainStartYear, trainEndYear))
+# axs[1].title.set_text('Test accuracy for different models (Games from {0}-{1})'.format(testStartYear, testEndYear))
+#
+# axs[1].set_xticks(range(1, t+2))
 
-
-        axs[0].plot(error, label='Model #' + str(t+1))
-        axs[0].set_xlabel('Epochs')
-        axs[0].set_ylabel('Training Accuracy')
-
-        axs[1].scatter(t+1, accuracy, label='Model #' + str(t+1))
-        axs[1].set_xlabel('Model #')
-        axs[1].set_ylabel('Test Accuracy')
-
-
-        # testAccuracy.append(accuracy)
-        print('Accuracy of the network on test set: Games from {0}-{1}: {2}%'.format(testStartYear, testEndYear, accuracy))
-
-
-    axs[0].legend(loc='upper left', bbox_to_anchor=(1.01, 1))
-    axs[1].legend(loc='upper left', bbox_to_anchor=(1.01, 1))
-
-    axs[0].title.set_text('Training accuracy for different models (Games from {0}-{1})'.format(trainStartYear, trainEndYear))
-    axs[1].title.set_text('Test accuracy for different models (Games from {0}-{1})'.format(testStartYear, testEndYear))
-
-    axs[1].set_xticks(range(1, t+2))
-
-    plt.tight_layout()
-    plt.savefig('Multiple Training Experiment')
+# plt.tight_layout()
+# plt.savefig('Multiple Taining Experiment')
 
