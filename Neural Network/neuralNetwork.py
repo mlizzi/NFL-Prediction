3231f70ab34f3bbc2e5bed37c674b794a6cfc25d
unknown <michael.lizzi@hotmail.com>
2024-04-19 21:13:50 -0400

    adjusting README and cleaning up file

--- Neural Network/neuralNetwork.py
+++ Neural Network/neuralNetwork.py
@@ -11,11 +11,14 @@ from matplotlib import pyplot as plt
 import time
 import csv
 from utils import getSeasonData, createTestSetLocation, pickleResults
+
 # from torchsummary import summary
 import openpyxl
-pd.set_option('display.width', 320)
-pd.set_option('display.max_columns', 20)
-pd.set_option('display.max_rows', 20)
+
+pd.set_option("display.width", 320)
+pd.set_option("display.max_columns", 20)
+pd.set_option("display.max_rows", 20)
+
 
 class neuralNetwork(nn.Module):
 
@@ -39,11 +42,17 @@ class neuralNetwork(nn.Module):
         out = self.fc3(out)
         return out
 
+
 class NFLDataset(Dataset):
 
     def __init__(self, dataFrame):
-        self.data = torch.tensor(dataFrame.drop('winnerATS', axis=1).values, dtype=torch.float)
-        self.target = torch.tensor(dataFrame['winnerATS'].values, dtype=torch.float).unsqueeze(1)
+        self.data = torch.tensor(
+            dataFrame.drop("winnerATS", axis=1).values, dtype=torch.float
+        )
+        self.target = torch.tensor(
+            dataFrame["winnerATS"].values, dtype=torch.float
+        ).unsqueeze(1)
+
     # This gets one row, ground truth https://pytorch.org/docs/stable/_modules/torchvision/datasets/mnist.html#MNIST
     def __getitem__(self, index):
         return (self.data[index], self.target[index])
@@ -52,39 +61,45 @@ class NFLDataset(Dataset):
         return self.data.shape[0]
 
 
-if __name__ == '__main__':
+def get_data(gameStats, teamStats):
 
-    ######################## SETUP STAGE ########################
     # Set data to use
-    indexStats = ['seasonNumber', 'week', 'home', 'away'] # Stats to index database by (not used in network)
-    gameStats = ['spread', 'winnerATS'] # Game data (used in network)
-    teamStats = ['CurrWins', 'TotalYards', 'Turnovers'] # Stats from both teams; prefix of "home" or "away" in database (used in network)
+    indexStats = [
+        "seasonNumber",
+        "week",
+        "home",
+        "away",
+    ]  # Stats to index database by (not used in network)
 
     # Choose which stats to normalize
-    normalizedStats = ['TotalYards', 'Turnovers', 'CurrWins', 'spread']
+    normalizedStats = ["TotalYards", "Turnovers", "CurrWins", "spread"]
 
     # Add prefix to team stats and normalize stats, set dataColumns to be columns pulled from pp_master database
 
     # Build dataColumns for columns used when reading database
     dataColumns = []
-    dataColumns += indexStats # must read index stats from database
+    dataColumns += indexStats  # must read index stats from database
     dataColumns += gameStats  # must read game stats from database
     for info in teamStats:
-        for loc in ['home', 'away']:
+        for loc in ["home", "away"]:
             dataColumns.append(loc + info)
 
     # Build normalizedColumns for columns to be normalized in database
     normalizedColumns = []
     for info in normalizedStats:
         if info in teamStats:
-            for loc in ['home', 'away']:
+            for loc in ["home", "away"]:
                 normalizedColumns.append(loc + info)
         else:
             normalizedColumns.append(info)
 
     # Read database
-    filePath = os.path.join(os.getcwd(), 'pp_master_boxscore_data_1970_2019.csv')
-    database = pd.read_csv(filePath, usecols=dataColumns, index_col=['seasonNumber', 'home', 'away', 'week'])
+    filePath = os.path.join(os.getcwd(), "pp_master_boxscore_data_1970_2019.csv")
+    database = pd.read_csv(
+        filePath,
+        usecols=dataColumns,
+        index_col=["seasonNumber", "home", "away", "week"],
+    )
 
     # store all stats used in network, including target
     usedStats = list(database.columns)
@@ -96,11 +111,171 @@ if __name__ == '__main__':
     for info in normalizedColumns:
         database[info] = (database[info] - database[info].mean()) / database[info].std()
 
+    return database
+
+
+def train_model(model, trainLoader, testLoader, loss, optimizer, epochs):
+    # Set train and test loss and accuracy to be appended to after each epoch
+    epochTrainLoss = []
+    epochTestLoss = []
+    epochTestAccuracy = []
+    epochTrainAccuracy = []
+
+    # Initialize variables to hold max accuracy of the test set
+    maxAccuracy = 0
+    maxAccuracyEpoch = 0
+
+    sigmoid = nn.Sigmoid()
+
+    for i in range(epochs):
+        batchTrainLoss = []
+        batchTestLoss = []
+        totalTrain = 0
+        correctTrain = 0
+        for j, (data, target) in enumerate(trainLoader):
+            model.train()
+            # Zero gradient and then predict
+            optimizer.zero_grad()
+            pred = model(data)
+            pred = sigmoid(pred)
+
+            # Compute loss
+            trainLoss = loss(pred, target)
+
+            # Append loss to list containing all losses in batch
+            batchTrainLoss.append(trainLoss.item())
+
+            # Calculate total number of batches run and total correct
+            totalTrain += target.size(0)
+            correctTrain += (torch.round(pred) == target).sum().item()
+
+            # propagate gradients and step in SGD
+            trainLoss.backward()
+            optimizer.step()
+
+        # Use average batch loss to save to total training loss for this epoch
+        epochTrainLoss.append(np.mean(batchTrainLoss))
+        trainAccuracy = correctTrain / totalTrain
+        epochTrainAccuracy.append(trainAccuracy)
+        # trainError.append(torch.mean((torch.round(pred) == target).float()))
+
+        totalTest = 0
+        correctTest = 0
+        with torch.no_grad():
+            for k, (data, target) in enumerate(testLoader):
+                model.eval()
+                # Compute prediction
+                pred = model(data)
+                pred = sigmoid(pred)
+
+                # Compute loss and append to list for this batch
+                testLoss = loss(pred, target)
+                batchTestLoss.append(testLoss.item())
+
+                # Calculate total number of batches run and total correct
+                totalTest += target.size(0)
+                correctTest += (torch.round(pred) == target).sum().item()
+
+            # testLoss.append(torch.mean((torch.round(pred) == target).float()))
+
+        epochTestLoss.append(np.mean(batchTestLoss))
+        testAccuracy = correctTest / totalTest
+        epochTestAccuracy.append(testAccuracy)
+        if (
+            testAccuracy > maxAccuracy and i > 3
+        ):  # Don't save accuracy for first few epochs (they are random)
+            maxAccuracy = testAccuracy
+            maxAccuracyEpoch = i
+
+        if i % 100 == 0:
+            print("Training Loss: " + str(np.mean(batchTrainLoss)))
+            print("Training Accuracy: " + str(trainAccuracy))
+            print("Testing Loss: " + str(np.mean(batchTestLoss)))
+            print("Testing Accuracy: " + str(testAccuracy))
+            print("-----------------------------------------")
+
+    plt.plot(epochTrainLoss)
+    plt.plot(epochTrainAccuracy)
+    plt.plot(epochTestLoss)
+    plt.plot(epochTestAccuracy)
+    plt.legend(["Training Loss", "Train Accuracy", "Test Loss", "Test Accuracy"])
+
+    resultsDict = {
+        "trainLoss": epochTrainLoss,
+        "trainAccuracy": epochTrainAccuracy,
+        "testLoss": epochTestLoss,
+        "testAccuracy": epochTestAccuracy,
+        "maxAccuracy": maxAccuracy,
+        "maxAccuracyEpoch": maxAccuracyEpoch,
+    }
+
+    return resultsDict
+
+
+def save_run():
+    # Save model
+    for i in range(1000):
+        modelFolder = "./Model Info/Model #{}".format(i)
+        if not os.path.exists(modelFolder):
+            plotName = "Model #{}.png".format(i)
+
+            os.makedirs(modelFolder)
+
+            plt.savefig(os.path.join(modelFolder, plotName))
+            plt.savefig("./Model Info/{}".format(plotName))
+            torch.save(
+                model.state_dict(),
+                os.path.join(modelFolder, plotName.replace(".png", ".pt")),
+            )
+            pickleResults(resultsDict, modelFolder, i)
+            break
+
+    print(
+        "Max Accuracy: {} at {}".format(
+            resultsDict["maxAccuracy"], resultsDict["maxAccuracyEpoch"]
+        )
+    )
+
+    # Allow user input from console for model run notes, save all information about run into csv file
+
+    notes = input("Notes about model?\n")
+    with open("./Model Info/models.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(
+            [
+                plotName,
+                "-".join(map(str, trainYears)),
+                "-".join(map(str, testYears)),
+                learning_rate,
+                epochs,
+                batchAmount,
+                resultsDict["maxAccuracy"],
+                resultsDict["maxAccuracyEpoch"],
+                preGameCount,
+                model,
+                gameStats + teamStats,
+                notes,
+            ]
+        )
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    gameStats = ["spread", "winnerATS"]  # Game data (used in network)
+    teamStats = [
+        "CurrWins",
+        "TotalYards",
+        "Turnovers",
+    ]  # Stats from both teams; prefix of "home" or "away" in database (used in network)
+
+    database = get_data(gameStats, teamStats)
+
     # Extract training and test sets
     # (originalTestData and originalTrainData hold raw data, testData and trainData hold calculated values done in future steps)
     trainYears = (2004, 2005)
     for year in range(2008, 2011):
-        testYears = (year,year)
+        testYears = (year, year)
         originalTrainData = getSeasonData(database, trainYears)
         originalTestData = getSeasonData(database, testYears)
         trainData = originalTrainData.copy(deep=True)
@@ -108,7 +283,11 @@ if __name__ == '__main__':
 
         # Build trainData and testData datasets using previous "preGameCount" number of games for each team
         preGameCount = 4
-        print("Creating Train and Test Datasets using past {} games...".format(preGameCount))
+        print(
+            "Creating Train and Test Datasets using past {} games...".format(
+                preGameCount
+            )
+        )
         start = time.time()
         trainData = createTestSetLocation(database, trainData, preGameCount)
         testData = createTestSetLocation(database, testData, preGameCount)
@@ -116,133 +295,35 @@ if __name__ == '__main__':
 
         # Set train and test loader to be used in training/test loops
         batchAmount = 3
-        trainLoader = DataLoader(NFLDataset(trainData), batch_size=len(trainData)//batchAmount)
-        testLoader = DataLoader(NFLDataset(testData), batch_size=len(testData)//batchAmount)
-
-        # Build model from neuralNetwork class
-        model = neuralNetwork(7, 75, 75, 1)
+        trainLoader = DataLoader(
+            NFLDataset(trainData), batch_size=len(trainData) // batchAmount
+        )
+        testLoader = DataLoader(
+            NFLDataset(testData), batch_size=len(testData) // batchAmount
+        )
 
         # loss = nn.CrossEntropyLoss()
         # https://www.youtube.com/watch?v=wpPkDSMzdKo>
         # weight = torch.tensor(np.arange(0, 1, 1/777)).unsqueeze(1)
         # loss = nn.BCELoss(weight=weight)
 
-        # Set loss, optimizer, and output activation
+        # Build model from neuralNetwork class and set training parameters
+        model = neuralNetwork(7, 75, 75, 1)
         loss = nn.BCELoss()
         learning_rate = 0.05
-        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, nesterov=True, momentum=0.9, dampening=0, weight_decay=0.01)
-        x = nn.Sigmoid()
-
-        # Set train and test loss and accuracy to be appended to after each epoch
-        epochTrainLoss = []
-        epochTestLoss = []
-        epochTestAccuracy = []
-        epochTrainAccuracy = []
-
-        # Initialize variables to hold max accuracy of the test set
-        maxAccuracy = 0
-        maxAccuracyEpoch = 0
-
+        optimizer = torch.optim.SGD(
+            model.parameters(),
+            lr=learning_rate,
+            nesterov=True,
+            momentum=0.9,
+            dampening=0,
+            weight_decay=0.01,
+        )
         epochs = 1000
-        for i in range(epochs):
-            batchTrainLoss = []
-            batchTestLoss = []
-            totalTrain = 0
-            correctTrain = 0
-            for j, (data, target) in enumerate(trainLoader):
-                model.train()
-                # Zero gradient and then predict
-                optimizer.zero_grad()
-                pred = model(data)
-                pred = x(pred)
 
-                # Compute loss
-                trainLoss = loss(pred, target)
+        # Train model
+        resultsDict = train_model(
+            model, trainLoader, testLoader, loss, optimizer, epochs
+        )
 
-                # Append loss to list containing all losses in batch
-                batchTrainLoss.append(trainLoss.item())
-
-                # Calculate total number of batches run and total correct
-                totalTrain += target.size(0)
-                correctTrain += (torch.round(pred) == target).sum().item()
-
-                # propagate gradients and step in SGD
-                trainLoss.backward()
-                optimizer.step()
-
-            # Use average batch loss to save to total training loss for this epoch
-            epochTrainLoss.append(np.mean(batchTrainLoss))
-            trainAccuracy = correctTrain / totalTrain
-            epochTrainAccuracy.append(trainAccuracy)
-            # trainError.append(torch.mean((torch.round(pred) == target).float()))
-
-
-            totalTest = 0
-            correctTest = 0
-            with torch.no_grad():
-                for k, (data, target) in enumerate(testLoader):
-                    model.eval()
-                    # Compute prediction
-                    pred = model(data)
-                    pred = x(pred)
-
-                    # Compute loss and append to list for this batch
-                    testLoss = loss(pred, target)
-                    batchTestLoss.append(testLoss.item())
-
-                    # Calculate total number of batches run and total correct
-                    totalTest += target.size(0)
-                    correctTest += (torch.round(pred) == target).sum().item()
-
-                # testLoss.append(torch.mean((torch.round(pred) == target).float()))
-
-            epochTestLoss.append(np.mean(batchTestLoss))
-            testAccuracy = correctTest / totalTest
-            epochTestAccuracy.append(testAccuracy)
-            if testAccuracy > maxAccuracy and i > 3: # Don't save accuracy for first few epochs (they are random)
-                maxAccuracy = testAccuracy
-                maxAccuracyEpoch = i
-
-            if i % 100 == 0:
-                print("Training Loss: " + str(np.mean(batchTrainLoss)))
-                print("Training Accuracy: " + str(trainAccuracy))
-                print("Testing Loss: " + str(np.mean(batchTestLoss)))
-                print("Testing Accuracy: " + str(testAccuracy))
-                print('-----------------------------------------')
-
-        plt.plot(epochTrainLoss)
-        plt.plot(epochTrainAccuracy)
-        plt.plot(epochTestLoss)
-        plt.plot(epochTestAccuracy)
-        plt.legend(['Training Loss', 'Train Accuracy', 'Test Loss', 'Test Accuracy'])
-
-        resultsDict = {'trainLoss': epochTrainLoss, 'trainAccuracy': epochTrainAccuracy, 'testLoss': epochTestLoss, 'testAccuracy': epochTestAccuracy}
-
-        # save plots to path
-
-
-        for i in range(1000):
-            modelFolder = './Model Info/Model #{}'.format(i)
-            if not os.path.exists(modelFolder):
-                plotName = 'Model #{}.png'.format(i)
-
-                os.makedirs(modelFolder)
-
-                plt.savefig(os.path.join(modelFolder, plotName))
-                plt.savefig("./Model Info/{}".format(plotName))
-                torch.save(model.state_dict(), os.path.join(modelFolder, plotName.replace('.png', '.pt')))
-                pickleResults(resultsDict, modelFolder, i)
-                break
-
-        print('Max Accuracy: {} at {}'.format(maxAccuracy, maxAccuracyEpoch))
-
-
-        # Allow user input from console for model run notes, save all information about run into csv file
-
-        notes = input("Notes about model?\n")
-        with open('./Model Info/models.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([plotName, "-".join(map(str, trainYears)), "-".join(map(str, testYears)), learning_rate, epochs, batchAmount, maxAccuracy, maxAccuracyEpoch, preGameCount, model, gameStats + teamStats, notes])
-
-        plt.show()
-
+        save_run()
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# NFL-Prediction
+# Time Dependency in NFL Against the Spread Predictions
+
+
+## Abstract
+Against the spread betting is a form of sports prediction aimed at the lucrative betting market in
+the National Football League. While recent revelations in the machine learning field have allowed
+for research in the application of neural networks for sports prediction, this paper aims to design
+new models meant specifically for increasing a bettor’s likelihood of profitability when placing
+against the spread bets. The paper attempts to improve bettor profitability by accounting for
+the passage of time during model creation. Through experimentation and analysis, this paper
+demonstrates the difficulties of developing against the spread models and suggests how this form
+of betting is often not in a bettor’s favour.
+
+## Repository Overview
+
+- Datasets are found in the Boxscore Scrapes folder
+- The neural network used to create predictions in this paper is found in neuralNetwork.py.
